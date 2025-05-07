b8b0ac33e64dada90f37471d30f452e3beded220
Sutharsi <sutharsiga2002@gmail.com>
2025-05-07 16:59:16 +0530

    NavBar and Third section homepage is created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>
-      <h1>Navbar sections</h1>
+    
+    <nav className="bg-white shadow">
+    <div className="max-w-screen-xl mx-auto px-4 flex justify-between items-center py-4">
+      <div className="text-xl font-bold text-black">LedgerVote</div>
+      <ul className="flex space-x-6 font-semibold">
+        <li><a href="#" className="text-blue-600">Home</a></li>
+        <li><a href="#" className="text-black hover:text-blue-600">Login</a></li>
+        <li><a href="#" className="text-black hover:text-blue-600">Register</a></li>
+        <li><a href="#" className="text-black hover:text-blue-600">FAQ</a></li>
+        <li><a href="#" className="text-black hover:text-blue-600">Contact Us</a></li>
+      </ul>
     </div>
+    
+  </nav>
+  
   )
 }
 
